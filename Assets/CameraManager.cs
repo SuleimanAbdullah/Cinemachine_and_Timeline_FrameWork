2d4822de1c79fd1c559cc6c85950a22b6eb8378d
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2023-01-29 14:10:35 +0300

    Create our cinematic sequence & Add ability to initiate cinematic camera

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -2,11 +2,10 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField]
-    private CinemachineVirtualCamera[] _cameras;
     [SerializeField]
     private int _currentCameraIndex;
     [SerializeField]
@@ -14,10 +13,19 @@ public class CameraManager : MonoBehaviour
 
     [SerializeField]
     private GameObject[] _shipViews;
-
+    [SerializeField]
+    private CinemachineVirtualCamera[] _cameras;
     private CinemachineVirtualCamera _currentCamera;
+
+    private Vector3 _temporaryMousePos;
+
+    private float _canPlayCinematicCamera = 10.0f;
+    private float _timeRate = 10.0f;
+    [SerializeField]
+    private PlayableDirector _cameracinematicDirector;
     private void Awake()
     {
+
         foreach (var _VirtualCamera in _cameras)
         {
             _currentCamera = _VirtualCamera.GetComponent<CinemachineVirtualCamera>();
@@ -27,17 +35,18 @@ public class CameraManager : MonoBehaviour
 
     void Start()
     {
-
         _currentCameraIndex = 0;
-       
+
         SetPriorityDefault();
         SetCurrentCamera();
         _isCurrentCameraActive = true;
+        _temporaryMousePos = Input.mousePosition;
     }
 
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.R) && _isCurrentCameraActive == true)
         {
             _shipViews[_currentCameraIndex].SetActive(false);
@@ -51,14 +60,43 @@ public class CameraManager : MonoBehaviour
 
             SetCurrentCamera();
         }
+
+        if (_temporaryMousePos == Input.mousePosition && Time.time >_canPlayCinematicCamera
+        || !Input.anyKeyDown && Time.time > _canPlayCinematicCamera)
+        {
+            if (_cameracinematicDirector.state == PlayState.Playing)
+            {
+                _canPlayCinematicCamera = Time.time + _timeRate;
+                return;
+            }
+            else
+            {
+                _cameracinematicDirector.gameObject.SetActive(true);
+                _shipViews[0].SetActive(true);
+                _cameracinematicDirector.Play();
+                _canPlayCinematicCamera = Time.time + _timeRate;
+                Debug.Log("Play Director:");
+            }
+        }
+
+
+        if (_temporaryMousePos != Input.mousePosition || Input.anyKeyDown)
+        {
+            _cameracinematicDirector.gameObject.SetActive(false);
+            _temporaryMousePos = Input.mousePosition;
+            _canPlayCinematicCamera = Time.time + _timeRate;
+        }
+
+        //when player hasnot moved the mouse 
+        //or pressed a key within 5 seconds
+        //have cinematic camera
     }
 
 
     public void SetPriorityDefault()
     {
-        Debug.Log("What is the problem");
-            _currentCamera.Priority = 10;
-            _currentCamera.Priority = 10;
+        _currentCamera.Priority = 10;
+        _currentCamera.Priority = 10;
         _isCurrentCameraActive = false;
     }
 
@@ -75,7 +113,6 @@ public class CameraManager : MonoBehaviour
                 _shipViews[_currentCameraIndex].SetActive(true);
                 _cameras[_currentCameraIndex].Priority = 15;
                 _isCurrentCameraActive = true;
-                Debug.Log("Third Person Camera");
                 break;
         }
     }
